ebd4818ee1d60a137f94dc287579e3c443d63d6e
Airfix982 <25roter52@gmail.com>
2023-03-22 21:54:33 +0400

    add: tree constructor

--- Lab_about_tree/Lab_about_tree/Tree.cpp
+++ Lab_about_tree/Lab_about_tree/Tree.cpp
@@ -1,1 +1,22 @@
-1 
+#include"Tree.hpp"
+
+Tree::Tree( int data )
+{
+
+	this->leaf.data = data;
+
+}
+
+Tree::Tree( const Tree& copied_tree )
+{
+
+	
+
+}
+
+void Tree::print() const
+{
+
+
+
+}
--- Lab_about_tree/Lab_about_tree/Tree.hpp
+++ Lab_about_tree/Lab_about_tree/Tree.hpp
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
 #pragma once
+struct Node
+{
+
+	int data;
+	Node* left;
+	Node* right;
+	Node( int data = 0, Node* left = NULL, Node*right = NULL ): data( data ), left( left ), right( right ) {};
+
+};
+
+
+class Tree
+{
+
+	private:
+		Node leaf;
+
+	public:
+
+		Tree( int data = 0 );
+
+		Tree( const Tree& copied_tree );
+
+		Tree& operator = ( const Tree& copied_tree );
+
+		void print() const;
+
+		bool insert( int key );
+
+		bool contains( int key ) const;
+
+		bool erase( int key );
+
+		~Tree();
+
+};
